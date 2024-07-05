e41d7a99d2774e14a5ee19baf125bb1f14414083
Raphael-08 <mohanvarma130@gmail.com>
2024-07-05 14:57:56 +0530

    updated package

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
 import { read, write, update, deleteFile, getLatestCommitSha, updateRef } from "./git-ops/git"
 export { read, write, update, deleteFile, getLatestCommitSha, updateRef }
-import { createdb, createCol, insert, startTransaction, transactionSuccess, rollBack } from "./db-ops/db"
-export { createdb, createCol, insert, startTransaction, transactionSuccess,rollBack }
+import { createdb, createCol, insert, findAll, updateMany, deleteMany, startTransaction, transactionSuccess, rollBack } from "./db-ops/db"
+export { createdb, createCol, insert, findAll, updateMany, deleteMany, startTransaction, transactionSuccess, rollBack }
