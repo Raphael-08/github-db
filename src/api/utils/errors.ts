2a30ae4ee5052ab5e639b9328b1fba4e4cd5fb10
Raphael-08 <mohanvarma130@gmail.com>
2024-07-11 22:32:58 +0530

    new error

--- src/api/utils/errors.ts
+++ src/api/utils/errors.ts
@@ -1,3 +1,6 @@
 export function EmptyTableError(message: string) {
     return { name: 'EmptyTableError', message };
 }
+export function UserMessedWithDBError(message: string){
+    return { name: 'UserMessedWithDBError', message };
+}
